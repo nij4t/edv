777093aefef731e38e4a048a23a61cf336284d16
Nijat Mahmudov <n.e.mahmudov@gmail.com>
2020-07-23 14:55:03 +0400

    add docs

--- pkg/api/v1alpha1/api.go
+++ pkg/api/v1alpha1/api.go
@@ -1,3 +1,24 @@
+// Package classification EDVGeriAlAPI
+//
+// This api provides a cashback service from EDV Geri Al
+//
+// Terms Of Service:
+//
+// there are no TOS at this moment, use at your own risk we take no responsibility
+//
+//     Schemes: http, https
+//     BasePath: /api/v1alpha1
+//     Version: 0.0.1
+//     License: MIT http://opensource.org/licenses/MIT
+//     Contact: Nijat Mahmudov<n.e.mahmudov@gmail.com>
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+// swagger:meta
 package v1alpha1
 
 import (
@@ -16,13 +37,29 @@ type Handler struct {
 	l logrus.FieldLogger
 }
 
+// LoginRequest is a request schema used for marshaling authentication
+// swagger:parameters login
 type LoginRequest struct {
-	Mobile   string `json:"mobile"`
-	Password string `json:"password"`
+	// in: body
+	Body struct {
+		// User's phone number
+		// required: true
+		Mobile string `json:"mobile"`
+		// User's Password
+		// required: true
+		Password string `json:"password"`
+	}
 }
 
+// LoginResponse is a response schema used when user provides correct credetials
+// swagger:response loginResponse
 type LoginResponse struct {
-	Token string `json:"token"`
+	// in: body
+	Body struct {
+		// Access Token
+		// required: true
+		Token string `json:"token"`
+	}
 }
 
 // Handler bounds urls to handle func
@@ -30,22 +67,30 @@ func (s *Handler) Handler(mux *http.ServeMux) {
 	mux.HandleFunc("/api/v1alpha1/login", s.Logger(s.Login))
 }
 
+// swagger:route POST /login login
+//
+// Authenticates user and returns JWT access token
+//
+// Responses:
+//   200: loginResponse
+//
 func (s *Handler) Login(w http.ResponseWriter, r *http.Request) {
-
 	dec := json.NewDecoder(r.Body)
 	defer r.Body.Close()
 	creds := &LoginRequest{}
-	err := dec.Decode(creds)
+	err := dec.Decode(&creds.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("invalid input data"))
 	}
 	s.l.Debugf("creds: %s", creds)
-	token := s.c.Login(creds.Mobile, creds.Password)
+	token := s.c.Login(creds.Body.Mobile, creds.Body.Password)
 
 	w.WriteHeader(http.StatusOK)
 	enc := json.NewEncoder(w)
-	payload := &LoginResponse{Token: token}
+	// payload := &LoginResponse{Token: token}
+	payload := &LoginResponse{}
+	payload.Body.Token = token
 	enc.Encode(payload)
 	return
 }
