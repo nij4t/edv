f6e10a4a6e0e4ff4c46b0270a94e617abc68cefc
Nijat Mahmudov <n.e.mahmudov@gmail.com>
2020-07-23 14:54:15 +0400

    add pong route

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -20,6 +20,10 @@ func main() {
 
 	mux := http.NewServeMux()
 	h.Handler(mux)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("PONG"))
+	})
 
 	addr := os.Getenv("EDV_ADDR")
 	if addr == "" {
