2e95120a8e8c057ba7efb71c862d25947fed314c
Nijat Mahmudov <n.e.mahmudov@gmail.com>
2020-07-27 19:41:16 +0400

    chore: remove unused class

--- qrcode-scanner/src/index.ts
+++ qrcode-scanner/src/index.ts
@@ -1,24 +1,15 @@
 import { Scanner } from "./Scanner"
 import { Camera } from "./Camera"
-import { Feed, Factory } from "./Feed"
 
 (async () => {
     const wrapper = document.getElementById('wrapper')
     const cam = new Camera()
 
-    // const feed = new Feed(wrapper, stream)
-    // const stream = await cam.getStream()
-
-    // const video = new Factory().make("video")
-    // video.srcObject = stream;
-
     const cameraFeed = await cam.requestVideo()
 
     const scanner = new Scanner()
     scanner.attachTo(wrapper)
-    // scanner.setSource(feed.getFeed())
 
     scanner.setSource(cameraFeed)
-    // scan on worker thread
     scanner.start()
 })()
