9ee798b549d92b054b95fb4d2eb1a47405c63763
Nijat Mahmudov <n.e.mahmudov@gmail.com>
2020-08-02 20:23:20 +0400

    feat: Create and mount scanner commponent

---
+++ qrcode-scanner/src/components/scanner.tsx
@@ -0,0 +1,39 @@
+import { Component, Fragment } from "preact";
+import { h } from 'preact'
+import * as libscanner from '../Scanner'
+import { Camera } from "../Camera";
+
+export class Scanner extends Component<Props, {}> {
+
+    private scanner: libscanner.Scanner
+
+    render() {
+        return (
+            <div id="scanner">
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        const cam = new Camera()
+        this.scanner = new libscanner.Scanner()
+        this.scanner.attachTo(document.querySelector('#scanner'))
+        this.scanner.onScan((data) => {
+            this.props.success(data)
+        })
+        cam.requestVideo().then(v => {
+            this.scanner.setSource(v)
+            this.scanner.start()
+        })
+    }
+
+    componentWillUnmount() {
+        this.scanner.stop()
+        this.scanner.source.srcObject = null
+    }
+
+}
+
+interface Props {
+    success(data: string): void
+}
--- qrcode-scanner/src/routes/home/index.tsx
+++ qrcode-scanner/src/routes/home/index.tsx
@@ -1,14 +1,23 @@
-import { h, Component } from 'preact'
+import { h, Component, Fragment } from 'preact'
 import { Button } from "preact-material-components/ts/Button"
+import { Scanner } from '../../components/scanner'
 
-export class Home extends Component<IProp, {}> {
+export class Home extends Component<IProp, State> {
     render() {
         return (
-            <Button onClick={this.props.logout}>Logout</Button>
+            <Fragment>
+                <Button onClick={this.props.logout}>Logout</Button>
+                <Scanner success={data => (this.props.success(data))}></Scanner>
+            </Fragment>
         )
     }
 }
 
+interface State {
+    fiskalId: string
+}
+
 interface IProp {
     logout(): void
+    success(payload: string): void
 }
